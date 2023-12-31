6955e295d8338d0a7633fb3d3ec1245dc8625158
Agung Satria <123456789+agungsatria890@gmail.com>
2023-12-31 23:59:03 +0700

    first commit

---
+++ dwmblocks/config.h
@@ -0,0 +1,9 @@
+//Modify this file to change what commands output to your statusbar, and recompile using the make command.
+static const Block blocks[] = {
+ /*Icon*/ 	/*Command*/	/*Update Interval*/	/*Update Signal*/
+  {"",      "sd-nettraf",      1,		            2},
+  {"",      "sd-volume",	     0,		            1},
+  {"",      "sd-battery",	     5,		            3},
+  {"",      "sd-clock",	       59,		          9},
+};
+static char *delim = " ";
